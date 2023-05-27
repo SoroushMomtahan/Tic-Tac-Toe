c28974b217885f2326e3872efd039fdabe5de77e
soroush momtahan <soroush_momtahan@outlook.com>
2023-05-27 15:50:22 +0330

    edit in vscode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [index, setIndex] = useState(0);
+
+    console.log('first ' + index);
+    function nameChangeHandle() {
+        setIndex(index + 1);
+        setIndex(index - 1);
+    }
+    console.log('end ' + index);
+    return (
+        <div>
+            <p onClick={nameChangeHandle}>{index}</p>
+            <p>{index}</p>
+        </div>
+    );
 }
-
-export default App;
