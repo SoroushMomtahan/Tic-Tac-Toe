1646765295ad40ef7682ef1212134fb1ad80cd0d
soroush momtahan <soroush_momtahan@outlook.com>
2023-05-27 16:13:18 +0330

    first time with vsc

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {useState} from "react";
+
+export default function App() {
+    const [index, setIndex] = useState(0);
+
+    console.log('first ' + index);
+    function nameChangeHandle() {
+        setIndex(index + 1);
+        setIndex(index - 1);
+    }
+    console.log('end ' + index);
+    return (
+        <div>
+            <p onClick={nameChangeHandle}>{index}</p>
+            <p>{index}</p>
+        </div>
+    );
+}
